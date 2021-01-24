5b7c66bb5ea755d3db23d5ded0833d4eacece83c
Josh Orr <jgorr@instructure.com>
2021-01-24 15:16:32 -0600

    basic movement

--- test/test.js
+++ test/test.js
@@ -9,24 +9,32 @@ describe("nothing", () => {
 describe("Elevator Control", () => {
   describe("Given an empty selected floor set", () => {
     it("should return an empty actions log", () => {
-      actionsLog = elevator.move(
-        0,
-        elevator.UP,
-        [],
-        []
-      );
+      actionsLog = elevator.move(0, elevator.UP, [], []);
       assert.notStrictEqual(actionsLog, []);
     });
   });
-  describe("Given a one element selected floor set", () => {
-    it("should return a single move and a single open");
-    expected = ["Move Up: 0, 1", "Door Open: 1"];
-    actionsLog = elevator.move(
-      1,
-      elevator.UP,
-      (selectedFloors = [1]),
-      (actionsLog = [])
-    );
-    assert.strictEqual(actionsLog, expected);
+  describe("Given a one element selected floor set, going up", () => {
+    it("should return a single move and a single open", () => {
+      expected = ["Move Up: 0, 1", "Door Open: 1"];
+      actual = elevator.move(
+        0,
+        elevator.UP,
+        (selectedFloors = [1]),
+        (actionsLog = [])
+      );
+      assert.notStrictEqual(actual, expected);
+    });
+  });
+  describe("Given a one element selected floor set, going down", () => {
+    it("should return a single move and a single open", () => {
+      expected = ["Move Down: 1, 0", "Door Open: 0"];
+      actual = elevator.move(
+        1,
+        elevator.UP,
+        (selectedFloors = [0]),
+        (actionsLog = [])
+      );
+      assert.notStrictEqual(actual, expected);
+    });
   });
 });
