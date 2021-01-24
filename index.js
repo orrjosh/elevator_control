4a37efd79e264b89706df6a063b57e790f5ed0e8
Josh Orr <orrjosh@gmail.com>
2021-01-24 14:25:17 -0600

    WIP

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello");
+// const readline = require('readline')
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// })
+
+// rl.question('What direction(up/down)', (direction) => {
+//   console.log(direction)
+// })
+
+console.log("to be implemented");
--- test/test.js
+++ test/test.js
@@ -1,7 +1,32 @@
-var assert = require("assert");
-
+const assert = require("assert");
+const elevator = require("../lib/elevator");
 describe("nothing", () => {
   it("should pass", () => {
     assert(true);
+  });
+});
+
+describe("Elevator Control", () => {
+  describe("Given an empty selected floor set", () => {
+    it("should return an empty actions log", () => {
+      actionsLog = elevator.move(
+        0,
+        elevator.UP,
+        [],
+        []
+      );
+      assert.notStrictEqual(actionsLog, []);
+    });
+  });
+  describe("Given a one element selected floor set", () => {
+    it("should return a single move and a single open");
+    expected = ["Move Up: 0, 1", "Door Open: 1"];
+    actionsLog = elevator.move(
+      1,
+      elevator.UP,
+      (selectedFloors = [1]),
+      (actionsLog = [])
+    );
+    assert.strictEqual(actionsLog, expected);
   });
 });
