5af425ac081315b7ef01a4e5fc6738bfa2d4343a
Josh Orr <jgorr@instructure.com>
2021-01-24 18:30:12 -0600

    broken test, going to try rewire

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const assert = require("assert");
 const elevator = require("../lib/elevator");
 
-describe("Elevator Control", () => {
+describe("Elevator Control Move", () => {
   describe("Given an empty selected floor set", () => {
     it("should return an empty actions log", () => {
       actionsLog = elevator.move(0, elevator.UP, [], []);
@@ -33,7 +33,7 @@ describe("Elevator Control", () => {
     });
   });
   describe("Given a selected floor set mixing directions", () => {
-    it("should return a single move and a single open", () => {
+    it("should go to the correct floors in order", () => {
       expected = [
         "Door Open: 1",
         "Move Up: 1, 2",
@@ -50,5 +50,27 @@ describe("Elevator Control", () => {
       );
       assert.deepEqual(actual, expected);
     });
+    it("should not matter if the set is in orderreturn a single move and a single open", () => {
+      expected = [
+        "Door Open: 1",
+        "Move Up: 1, 2",
+        "Door Open: 2",
+        "Move Down: 2, 1",
+        "Move Down: 1, 0",
+        "Door Open: 0",
+      ];
+      actual = elevator.move(
+        1,
+        elevator.UP,
+        (selectedFloors = [1, 0, 2]),
+        (actionsLog = [])
+      );
+      assert.deepEqual(actual, expected);
+    });
+  });
+});
+describe("Elevator Control Move isSelectedFloorsValid", () => {
+  it("should return false if lowest floor less than bottom floor", () => {
+    assert.deepEqual(elevator.isSelectedFloorsValid([-2]), false);
   });
 });
